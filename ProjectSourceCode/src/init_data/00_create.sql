d89e8b6422354d48281f8e44f8053544735f5e33
Shivam Pathak <shivam.pathak@ucdenver.edu>
2025-07-21 20:00:03 -0600

    Test

--- ProjectSourceCode/src/init_data/00_create.sql
+++ ProjectSourceCode/src/init_data/00_create.sql
@@ -1,6 +1,13 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  username VARCHAR(50) PRIMARY KEY,
-  password CHAR(60) NOT NULL
+  user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(100) UNIQUE NOT NULL,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password CHAR(60) NOT NULL,
+  bio TEXT,
+  profile_photo TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

5d6b25e3ae28dd36579a3fc7a4d93c796b9fc62b
Shivam Pathak <shivam.pathak@ucdenver.edu>
2025-07-21 19:52:28 -0600

    Test

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -52,7 +52,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // serve u
 // --- DATABASE ---
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
+  port: process.env.DB_PORT || 5432,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
